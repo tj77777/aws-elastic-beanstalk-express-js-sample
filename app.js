0a8cb0552897f26cfa0e66a7cfc1600d030a860c
Tharindu Jayasooriya <tharindujayasooriya@gmail.com>
2024-11-10 01:21:36 +1300

    change the outout yo yo

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('This is the Frist Code PipLine'));
+app.get('/', (req, res) => res.send('Im Going to win this thing'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
-ß
