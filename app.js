29a2067781a674f156ab2689a66af585a7a2752e
Tharindu Jayasooriya <tharindujayasooriya@gmail.com>
2024-11-09 22:52:17 +1300

    change the outout file

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('This is the Frist Code PipLine'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
+ß
